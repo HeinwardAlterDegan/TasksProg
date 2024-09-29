5b879fc6ec5117c25eee3e4becb477e1d03a219d
Эдуард Гусев <f1ful1337@gmail.com>
2024-09-29 16:52:08 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1Poetry
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Песнь Касильды");
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Горит звезда, черным-черна,");
+            Console.WriteLine("Кружится странная луна");
+            Console.WriteLine("Здесь, над затерянной");
+            Console.WriteLine("Каркозой.");
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Роберт Чамберс, Король в Жёлтом.");
+            Console.ReadKey();
+        }
+    }
+}
